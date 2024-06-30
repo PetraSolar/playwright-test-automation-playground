c87b7f566c36b19c4902b2660a4f14015322e175
Petra Solarská <solarska.petra@gmail.com>
2024-06-30 13:48:15 +0200

    Test of ButtonPage

--- pages/buttons.ts
+++ pages/buttons.ts
@@ -4,7 +4,6 @@ export class ButtonPage {
   readonly page: Page;
   readonly navigationBar: string;
   readonly header: Locator;
-
   readonly buttonById: Locator;
   readonly buttonByName: Locator;
   readonly buttonByClass: Locator;
@@ -12,36 +11,29 @@ export class ButtonPage {
   readonly buttonByXPath: Locator;
   readonly buttonByPartialText: Locator;
   readonly buttonByDataQA: Locator;
-
   readonly buttonOutsideDiv: Locator;
+  readonly expectToAllButtonsAreClicked: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.navigationBar = "nav";
     this.header = page.getByRole("heading", { name: "Tlačítka" });
-
-    // OH! Fill this! Check the HTML and fill the selectors. Use whatever selector you want.
     this.buttonById = page.locator("#button1");
-    this.buttonByName;
-    this.buttonByClass;
-    this.buttonByText;
-    this.buttonByXPath;
-    this.buttonByPartialText;
-    this.buttonByDataQA;
-
-    this.buttonOutsideDiv;
+    this.buttonByName = page.locator('[name="clickingOnMe"]');
+    this.buttonByClass = page.locator('.click-click-click');
+    this.buttonByText = page.getByText("This is THE button");
+    this.buttonByXPath = page.getByText("XPATH");
+    this.buttonByPartialText = page.getByText("Partial link text", { exact: true });
+    this.buttonByDataQA = page.getByTestId("customAttribute-button");
+    this.buttonOutsideDiv = page.getByRole("button", { name: "Button mimo" });
+    this.expectToAllButtonsAreClicked = page.locator('.btn-success');
   }
 
   async visit() {
     await this.page.goto("/selectorsButtons.html");
-  }
+  }}
 
-  async expectToAllButtonsAreClicked() {
-    // this.header.waitFor({ state: "visible" });
-    const buttons = this.page.getByRole("button");
-
-    for (const button of await buttons.all()) {
-      await expect.soft(button).toHaveClass("btn-success", { timeout: 100 });
-    }
-  }
-}
+  /* async function expectToAllButtonsAreClicked() {
+    const successButtonsCount = await this.page.locator('.btn-success').count();
+    expect(successButtonsCount).toBe(8);
+  }*/
